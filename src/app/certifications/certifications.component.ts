ba6ba0cccfd07838845ea45628d8c00616025a60
André Masson <amwebexpert@gmail.com>
2017-09-04 12:16:51 -0400

    Added multiple menu items

---
+++ src/app/certifications/certifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-certifications',
+  templateUrl: './certifications.component.html',
+  styleUrls: ['./certifications.component.css']
+})
+export class CertificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/technologies/technologies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies',
+  templateUrl: './technologies.component.html',
+  styleUrls: ['./technologies.component.css']
+})
+export class TechnologiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
