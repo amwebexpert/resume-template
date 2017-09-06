044d7ddd7f0f8654ee0c4c6ac6888c7fffa140f3
André Masson <amwebexpert@gmail.com>
2017-09-06 07:09:07 -0400

    Added polyfils for IE11

--- build.cmd
+++ build.cmd
@@ -4,6 +4,16 @@ echo ng build --prod
 echo ------------------------------------------------------------------------------
 call ng build --prod
 
-echo "copy all file from C:\Dev\resume-template\dist and paste them to destination"
-rem xcopy C:\Dev\resume-template\dist\ C:\DONNES~1\AMWEBE~2\ /O /X /E /H /K
+echo "Removing all files from target"
+del /q \DONNES~1\AMWEBE~2\*.css
+del /q \DONNES~1\AMWEBE~2\*.js
+del /q \DONNES~1\AMWEBE~2\*.html
 
+echo "Copying all files from \dist"
+copy .\dist\*.css \DONNES~1\AMWEBE~2\
+copy .\dist\*.js \DONNES~1\AMWEBE~2\
+copy .\dist\*.html \DONNES~1\AMWEBE~2\
+
+echo ------------------------------------------------------------------------------
+echo "Done."
+echo ------------------------------------------------------------------------------
