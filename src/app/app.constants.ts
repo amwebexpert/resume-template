20955b557b061b1d77aba9831b63d3c0ca44203d
André Masson <amwebexpert@gmail.com>
2018-04-07 10:51:44 -0400

    Updated

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,7 +3,7 @@
  */
 export class AppConstants {
 
-  public static DATE = '2017-09-03';
+  public static DATE = '2018-04-07';
   public static APP_TITLE = 'André Masson';
   public static APP_TITLE_VERSION: string = AppConstants.APP_TITLE + ' - ' + AppConstants.DATE;
 
