540aa258c8bf98f1136cf7a17ba2fab65fdf9290
André Masson <amwebexpert@gmail.com>
2019-07-01 10:48:28 -0400

    Upgrade 2019-07
    Updated with latest knowledges

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -7,7 +7,7 @@ declare var $: any; // jQuery reference
  */
 export class AppConstants {
 
-  public static DATE = '2019-03-23';
+  public static DATE = '2019-07-01';
   public static APP_TITLE = 'AM';
   public static APP_TITLE_VERSION: string = AppConstants.APP_TITLE + ' - ' + AppConstants.DATE;
 
