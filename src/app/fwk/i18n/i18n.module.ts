5365482ee3e70714441eaec344a8e5c65fc4bcfa
amwebexpert <amwebexpert@gmail.com>
2021-02-21 11:44:48 -0500

    Upgraded to Angular 9

--- src/app/fwk/i18n/i18n.module.ts
+++ src/app/fwk/i18n/i18n.module.ts
@@ -1,5 +1,7 @@
 import { MissingTranslationHandler, MissingTranslationHandlerParams } from '@ngx-translate/core';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class AppMissingTranslationHandler implements MissingTranslationHandler {
     handle(params: MissingTranslationHandlerParams) {
         return params.key;
