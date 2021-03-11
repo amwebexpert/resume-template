c5bfb3985a9c232d3782219e3eb266bed91e0a70
amwebexpert <amwebexpert@gmail.com>
2021-03-11 09:17:06 -0500

    Renamed navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AppConstants } from '../app.constants';
 
 @Component({
-  selector: 'app-top-horizontal-navbar',
-  templateUrl: './top-horizontal-navbar.component.html',
-  styleUrls: ['./top-horizontal-navbar.component.css']
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
-export class TopHorizontalNavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit {
 
   appTitleVersion = AppConstants.APP_TITLE_VERSION;
   authorName = AppConstants.AUTHOR_NAME;
